9e1ca84e846fbafd3cb22ee980bce2e8fc38a20a
Pratheeba S <pratheebas887@gmail.com>
2026-01-27 14:38:06 +0530

    Javascript

---
+++ samp.java
@@ -0,0 +1,13 @@
+public class samp {
+    static void add(){
+        System.out.println("hi");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        samp.add();
+
+    }
+
+    
+    
+}
