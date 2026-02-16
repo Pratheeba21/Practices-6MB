3d0d2435137b763d491a16c638877506f114abed
Pratheeba S <pratheebas887@gmail.com>
2026-02-16 10:23:53 +0530

    Javascript
    Signed-off-by: Pratheeba S <pratheebas887@gmail.com>

---
+++ arrayss.js
@@ -0,0 +1,6 @@
+const arr=[1,2,3];
+//0 n
+arr=[5,6,7];
+console.log(arr);
+
+//[Ift4f@
---
+++ arrow_func.js
@@ -0,0 +1,32 @@
+let a=10;
+let b=20;
+
+//1st - sum
+// const add= (e,f) => {
+//     const sum=e+f;
+//     return sum
+// };
+
+//2nd - square
+// let y=5;
+// const squ=y=>y*y;
+// console.log(squ);
+
+//3rd
+
+// const greet=()=>{
+// console.log("Hello world");
+
+// console.log("Hello ");
+// }
+// greet();
+const multiple=(a,b)=>{
+    let result=a*b;
+    console.log(`multip: ${result}`);
+}
+multiple(6,7);
+
+//template literal
+// f'{dghu {var_namr}}
+
+// `multip: ${result}`
